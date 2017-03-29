c485e834925ead1a6b5e8602a10773421e999ec9
mlu27 <mlu27@i3-dhcp-172-16-55-189.icecube.wisc.edu>
2017-03-28 21:03:20 -0500

    Define 2017 Jan 24 - 25 IC deep pulser run times (A2 run8573) in calibrationTools.h. Create analysis_2017DeepPulser.cxx to specifically reconstruct events in those periods.

--- calibrationTools.h
+++ calibrationTools.h
@@ -31,9 +31,16 @@ using namespace std;
                               //run1946 from 1388840284 - 1388861817
                               //This run covers 2014 Jan. 4 16:00 GMT. After this time ARA03 D4 should be discarded
 
+//Unix timestamp of 2017 Jan 24 23:48- Jan 25 00:00 (UTC) IC deep pulser string 1 shallow pulser operation. This is in ARA2 run8573
+#define deepPulserString1StartTime_2017 1485301680
+#define deepPulserString1EndTime_2017 1485302400
+//Unix timestamp of 2017 Jan 25 00:01- Jan 25 00:19 (UTC) IC deep pulser string 22 shallow pulser operation. This is in ARA2 run8573
+#define deepPulserString22StartTime_2017 1485302460
+#define deepPulserString22EndTime_2017 1485303540
+
 //void invertGraph(TGraph *gr);
-int calibrateGeometryAndDelays(const RawAraStationEvent *rawAtriEvPtr, 
-                                double (&posDelayArray)[4][4], double *pulserCorr, 
+int calibrateGeometryAndDelays(const RawAraStationEvent *rawAtriEvPtr,
+                                double (&posDelayArray)[4][4], double *pulserCorr,
                                 const float stationCenterDepth,
                                 vector<vector<double> >& ant_loc,
                                 vector<vector<double> >& pul_loc);
