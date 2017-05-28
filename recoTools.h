dbb7a1191e1410a54d58c9bc7080409ae843dfc2
mlu27 <mlu27@harveystreet-127-211.resnet.wisc.edu>
2017-05-28 14:16:14 -0500

    Discard struct timer completely

--- recoTools.h
+++ recoTools.h
@@ -398,6 +398,6 @@ int doNchnlScan(const double eventWeight, const vector<TGraph *>& cleanEvent, TH
 int doNchnlScan(const double eventWeight, const vector<TGraph *>& cleanEvent, TH2F *mnMap, TH2F *mnMap_V, TH2F *mnMap_H, int *nchnlArray, const int *chanMask, int *goodChan, const int nThresStep = 250, const double minThres = 0., const double maxThres = 10.);
 int computeMapLikelihoodAndPValue(const int nDir, const int nLayer, const char *fitFunc, const char *fitFuncFile, float *mapData, double& likelihood, double& pValue);
 double getPeakSqValRange(TGraph *gr, int *index, int firstBin, int lastBin);
-int recordTime(timer *t, const int count);
-void resetTimer(timer *t);
+//int recordTime(timer *t, const int count);
+//void resetTimer(timer *t);
 #endif
