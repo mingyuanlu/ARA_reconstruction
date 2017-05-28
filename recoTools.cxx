a12064c87a296c16ddc2fc79109859103c53a35f
mlu27 <mlu27@harveystreet-127-211.resnet.wisc.edu>
2017-05-28 12:56:46 -0500

    Explicitly record return value from clock()

--- recoTools.cxx
+++ recoTools.cxx
@@ -10342,7 +10342,8 @@ if(count != t->clockVec.size() /*|| count != t->chronoVec.size()*/ || count != t
 
 }
 cout<<"10344\n";
-t->clockVec.push_back(clock());
+clock_t cnow = clock();
+t->clockVec.push_back(cnow);
 cout<<"10346\n";
 //t->chronoVec.push_back(std::chrono::monotonic_clock::now());
 t->timeVec.push_back(time(0));
