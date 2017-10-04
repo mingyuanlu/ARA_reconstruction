0df5a42dac8f86f9c6b2b962a2bfa83183847687
mlu27 <mlu27@Tiros.local>
2017-10-04 17:40:42 -0500

    Debug

--- recoTools.cxx
+++ recoTools.cxx
@@ -7051,6 +7051,8 @@ cvs.SaveAs("testPValueDist.C");
 
 cout<<"Creating Healpix map and writing to FITS....\n";
 //arr<float> MArr = arr<float>(&M[0], (size_t)nDir);
+cout<<"maxPixIdx: "<<rank[0]<<" nDir: "<<nDir<<endl;
+cout<<"layer: "<<rank[0]/nDir<<endl;
 arr<float> MArr = arr<float>(&M[rank[0] / nDir], (size_t)nDir);
 Healpix_Map<float> skyMap = Healpix_Map<float>(MArr, HEALPIX_ORDERING);
 
