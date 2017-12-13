d41429915efba93b4572c205da6824b1e5673d4c
mlu27 <mlu27@dyn-13-204.icecube.wisc.edu>
2017-12-13 13:06:58 -0600

    Add trackEngine class

---
+++ trackEngine.cxx
@@ -0,0 +1,479 @@
+#include "trackEngine.h"
+#include "TObject.h"
+
+ClassImp(trackEngine)
+
+using namespace std;
+
+trackEngine::trackEngine(){}
+
+trackEngine::~trackEngine(){
+
+  clear();
+
+}
+
+int trackEngine::buildBaselineTracks(const vector< vector<double> >& antLocation){
+
+/*
+   int numStr = (int)detector->stations[0].size();
+   int numAnt = (int)detector->stations[0].strings[0].size();
+   cout<<"Number of strings: "<<numStr<<" Number of antennas per string: "<<numAnt<<endl;
+*/
+
+   int nAnt = (int)antLocation.size();
+   if(nAnt <= 0){ cerr<<"antLocation wrong size\n"; return -1; }
+   Vector uVec;
+   vector<Vector> tempVec;
+   vector<double> tempTime;
+   double dx, dy, dz, norm, dt;
+
+   for(int ant1=0; ant1<nAnt; ant1++){
+
+     tempVec.clear();
+     tempTime.clear();
+     for(int ant2=0; ant2<nAnt; ant2++){
+
+       dx = antLocation[ant2][0] - antLocation[ant1][0];
+       dy = antLocation[ant2][1] - antLocation[ant1][1];
+       dz = antLocation[ant2][2] - antLocation[ant1][2];
+       uVec.SetXYZ(dx, dy, dz);
+       cout<<"ant1: "<<ant1<<" ant2: "<<ant2<<" norm "<<uVec.Mag()<<endl;
+       uVec.Print();
+       dt = uVec.Mag() * nIce / speedOfLight;
+       tempTime.push_back(dt);
+       tempVec.push_back(uVec.Unit());
+       cout<<"Unit norm: "<<tempVec[ant2].Mag()<<" tempTime: "<<tempTime[ant2]<<endl;
+
+      }
+   baselineTrackTimes.push_back(tempTime);
+   baselineTracks.push_back(tempVec);
+   }
+
+   if(checkBaselineTracks() > 1e-9 ){ cerr<<"Sum of all baseline tracks !=0!\n"; return -1;}
+
+   return 0;
+}
+
+int trackEngine::computeFinalTracks(vector<TGraph* > unpaddedEvent){
+
+   double peakTArray[16];
+   float snrArray[16];
+   int index[16];
+   recoTools::getChannelSNR(unpaddedEvent, snrArray);
+   TMath::Sort(16,snrArray,index);
+   getChannelPeakTime(unpaddedEvent, peakTArray);
+
+   int nAnt = (int)unpaddedEvent,size();
+   //int nTracks = TMath::Binomial(nAnt, 2);
+   //cout<<"nTracks: "<<nTracks<<endl;
+   trackRank = (int*)calloc(nAnt*nAnt, sizeof(int));
+   trackSNRArray = (float*)calloc(nAnt*nAnt, sizeof(float));
+
+   for(int ant1=0; ant1<nAnt; ant1++){
+      for(int ant2=0; ant2<nAnt; ant2++){
+        trackSNRArray[ant1*nAnt+ant2] = (snrArray[ant1]+snrArray[ant2]) / 2.; //Take the average SNR as the pair SNR
+      }
+   }
+
+   TMath::Sort(nAnt*nAnt, trackSNRArray, trackRank);
+
+   double cosine;
+   goodTrackCount = 0;
+   badTrackCount  = 0;
+   cosine = (double*)malloc(sizeof(double)*nAnt*nAnt);
+
+   for(int anti=0; anti<nAnt; anti++){
+     for(int antf=0; antf<nAnt; antf++){
+
+       if(peakTArray[anti]>-1e9 && peakTArray[antf]>-1e-9){
+         if(fabs(peakTArray[anti]-peakTArray[antf]) < baselineTrackTimes[anti][antf] + tolerance){
+
+           goodTrackCount++;
+
+           //if(peakTArray[anti] > peakTArray[antf]){
+
+          //   cosine[anti*nAnt+antf] = -1.* (peakTArray[anti] - peakTArray[antf]) / baselineTrackTimes[anti][antf];
+
+           //} else {
+
+           cosine[anti*nAnt+antf] = (peakTArray[antf] - peakTArray[anti]) / baselineTrackTimes[anti][antf];
+
+           //}
+           demoFinalTrack += (baselineTracks[anti][antf] * cosine[anti*nAnt+antf]);
+
+         } else {
+
+           badTrackCount++;
+           cosine[anti*nAnt+antf] = -1e9;
+
+         }//end of else
+       }//end of if t > -1e9
+       else {
+         badTrackCount++;
+         cosine[anti*nAnt+antf] = -1e9;
+       }
+     }//end of antf
+   }//end of anti
+
+   demoFinalTrack = demoFinalTrack / 2.; //acoount for double counting
+   Vector tempVector;
+   tempVector.SetXYZ(0.,0.,0.);
+   for(int rank=0; rank<nAnt*nAnt; rank++){
+
+     anti = trackRank[rank]/nAnt;
+     antf = trackRank[rank]%nAnt
+
+     if(cosine[anti*nAnt+antf] > -1e9){
+     if( (tempVector+baselineTracks[anti][antf]*cosine[anti*nAnt+antf]).Mag() > tempVector.Mag() ){
+
+       tempVector += baselineTracks[anti][antf]*cosine[anti*nAnt+antf];
+
+     }
+     }
+   }//end of rank
+
+   if(tempVector.Mag() < 1e-9){ cerr<<"hierFinalTrack lenght is zero!\n"; return -1;}
+   else hierFinalTrack = (tempVector / 2.); //account for double counting
+
+   return 0;
+}
+
+int trackEngine::getChannelPeakTime(vector<TGraph *> unpaddedEvent, double *peakTArray){
+
+  double peakV, peakT, v, t;
+  peakV = 0.;
+  peakT = -1e10;
+
+   for(int ch=0; ch<unpaddedEvent.size(); ch++){
+      for(int i=0; i<unpaddedEvent[ch]->GetN(); i++){
+
+         unpaddedEvent[ch]->GetPoint(i, t, v);
+         if( fabs(v) > peakV ){ peakV = fabs(v); peakT = t; }
+
+      }
+      peakTArray[ch] = peakT;
+   }
+
+   return 0;
+}
+
+double checkBaselineTracks(){
+
+  Vector tempVector;
+  tempVector.SetXYZ(0.,0.,0.);
+
+  for(int ant1=0; ant1<baselineTracks.size(); ant1++){
+    for(int ant2=0; ant2<baselineTracks.size(); ant2++){
+
+      tempVector += baselineTracks[ant1][ant2];
+
+    }
+  }
+  return tempVector.Mag();
+}
+
+/*
+int computeExtrapFinalTracksWithDemo(){
+
+  vector<Vector> tempVector;
+  Vector temp;
+  double rotAngle;
+  int nAnt = (int)baselineTracks.size();
+
+  for(int anti=0; anti<nAnt; ant1++){
+
+    tempVector.clear();
+    for(int antf=0; antf<nAnt; ant2++){
+      rotAngle = TMath::Pi()/2. - demoFinalTrack.Angle(baselineTracks[anti][antf]);
+      temp = demoFinalTrack.Rotate(rotAngle, baselineTracks[anti][antf].Cross(demoFinalTrack));
+      temp = temp / temp.Mag();
+      temp = temp * sqrt(1-cosine[anti*nAnt+antf]*cosine[anti*nAnt+antf]);
+      tempVector.push_back(temp);
+    }
+    eventOrthoTracks.push_back(tempVector);
+  }
+
+  demoExtrapFinalTrack = (demoFinalTrack * 2.)
+
+  for(int anti=0; anti<nAnt; anti++){
+    for(int antf=0; antf<nAnt; antf++){
+
+      if(cosine[anti*nAnt+antf]> -1e9){
+        demoExtrapFinalTrack += eventOrthoTracks[anti][antf];
+      }
+    }
+  }
+
+  demoExtrapFinalTrack = demoExtrapFinalTrack / 2.;
+
+  Vector tempVector;
+  tempVector.SetXYZ(0.,0.,0.);
+  for(int rank=0; rank<nAnt*nAnt; rank++){
+
+    anti = trackRank[rank]/nAnt;
+    antf = trackRank[rank]%nAnt
+
+    if(cosine[anti*nAnt+antf] > -1e9){
+    if( (tempVector
+        + baselineTracks[anti][antf]*cosine[anti*nAnt+antf]
+        + eventOrthoTracks[anti][antf]
+        ).Mag() > tempVector.Mag() ){
+
+      tempVector += (baselineTracks[anti][antf]*cosine[anti*nAnt+antf] + eventOrthoTracks[anti][antf]);
+
+    }
+    }
+  }//end of rank
+
+  if(tempVector.Mag() < 1e-9){ cerr<<"hierExtrapFinalTrack lenght is zero!\n"; return -1;}
+  else hierExtrapFinalTrack = (tempVector / 2.); //account for double counting
+
+  return 0;
+}
+
+int computeExtrapFinalTracksWithHier(){
+
+  vector<Vector> tempVector;
+  Vector temp;
+  double rotAngle;
+  int nAnt = (int)baselineTracks.size();
+
+  for(int anti=0; anti<nAnt; ant1++){
+
+    tempVector.clear();
+    for(int antf=0; antf<nAnt; ant2++){
+      rotAngle = TMath::Pi()/2. - hierFinalTrack.Angle(baselineTracks[anti][antf]);
+      temp = hierFinalTrack.Rotate(rotAngle, baselineTracks[anti][antf].Cross(hierFinalTrack));
+      temp = temp / temp.Mag();
+      temp = temp * sqrt(1-cosine[anti*nAnt+antf]*cosine[anti*nAnt+antf]);
+      tempVector.push_back(temp);
+    }
+    eventOrthoTracks.push_back(tempVector);
+  }
+
+  demoExtrapFinalTrack = (demoFinalTrack * 2.)
+
+  for(int anti=0; anti<nAnt; anti++){
+    for(int antf=0; antf<nAnt; antf++){
+
+      if(cosine[anti*nAnt+antf]> -1e9){
+        demoExtrapFinalTrack += eventOrthoTracks[anti][antf];
+      }
+    }
+  }
+
+  demoExtrapFinalTrack = demoExtrapFinalTrack / 2.;
+
+  Vector tempVector;
+  tempVector.SetXYZ(0.,0.,0.);
+  for(int rank=0; rank<nAnt*nAnt; rank++){
+
+    anti = trackRank[rank]/nAnt;
+    antf = trackRank[rank]%nAnt
+
+    if(cosine[anti*nAnt+antf] > -1e9){
+    if( (tempVector
+        + baselineTracks[anti][antf]*cosine[anti*nAnt+antf]
+        + eventOrthoTracks[anti][antf]
+        ).Mag() > tempVector.Mag() ){
+
+      tempVector += (baselineTracks[anti][antf]*cosine[anti*nAnt+antf] + eventOrthoTracks[anti][antf]);
+
+    }
+    }
+  }//end of rank
+
+  if(tempVector.Mag() < 1e-9){ cerr<<"hierExtrapFinalTrack lenght is zero!\n"; return -1;}
+  else hierExtrapFinalTrack = (tempVector / 2.); //account for double counting
+
+  return 0;
+}
+*/
+//int computeExtrapFinalTracks(Vector finalTrack){
+int buildEventOrthoTracks(Vector finalTrack){
+
+  if(finalTrack.Mag() == 0){ cerr<<"No finalTrack!\n"; return -1;}
+  vector<Vector> tempVector;
+  Vector temp;
+  double rotAngle;
+  int nAnt = (int)baselineTracks.size();
+  if(nAnt == 0){ cerr<<"No baselineTracks!\n"; return -1;}
+
+  for(int anti=0; anti<nAnt; anti++){
+
+    tempVector.clear();
+    for(int antf=0; antf<nAnt; antf++){
+      rotAngle = TMath::Pi()/2. - finalTrack.Angle(baselineTracks[anti][antf]);
+      temp = finalTrack.Rotate(rotAngle, baselineTracks[anti][antf].Cross(finalTrack));
+      temp = temp / temp.Mag();
+      temp = temp * sqrt(1-cosine[anti*nAnt+antf]*cosine[anti*nAnt+antf]);
+      tempVector.push_back(temp);
+    }
+    eventOrthoTracks.push_back(tempVector);
+  }
+
+  return 0;
+}
+
+Vector computeDemoExtrapFinalTrack(){
+
+  if(demoFinalTrack.Mag()==0 || demoFinalTrack==NULL){ cerr<<"No demoFinalTrack\n"; return NULL;}
+  //demoExtrapFinalTrack = (demoFinalTrack * 2.)
+  Vector deft = (demoFinalTrack * 2.)
+
+  if(eventOrthoTracks.size()==0){ cerr<<"No eventOrthoTracks\n"; return NULL;}
+
+  int nAnt = (int)baselineTracks.size();
+
+  for(int anti=0; anti<nAnt; anti++){
+    for(int antf=0; antf<nAnt; antf++){
+
+      if(cosine[anti*nAnt+antf]> -1e9){
+        /*demoExtrapFinalTrack*/deft += eventOrthoTracks[anti][antf];
+      }
+    }
+  }
+
+  //demoExtrapFinalTrack = demoExtrapFinalTrack / 2.;
+  deft = deft / 2.;
+
+  //return demoExtrapFinalTrack;
+  return deft;
+}
+
+Vector computeHierExtrapFinalTrack(){
+
+  if(eventOrthoTracks.size()==0){ cerr<<"No eventOrthoTracks\n"; return NULL;}
+  int nAnt = (int)baselineTracks.size();
+  Vector tempVector;
+  tempVector.SetXYZ(0.,0.,0.);
+  for(int rank=0; rank<nAnt*nAnt; rank++){
+
+    anti = trackRank[rank]/nAnt;
+    antf = trackRank[rank]%nAnt
+
+    if(cosine[anti*nAnt+antf] > -1e9){
+    if( (tempVector
+        + baselineTracks[anti][antf]*cosine[anti*nAnt+antf]
+        + eventOrthoTracks[anti][antf]
+        ).Mag() > tempVector.Mag() ){
+
+      tempVector += (baselineTracks[anti][antf]*cosine[anti*nAnt+antf] + eventOrthoTracks[anti][antf]);
+
+    }
+    }
+  }//end of rank
+
+  if(tempVector.Mag() < 1e-9){ cerr<<"hierExtrapFinalTrack lenght is zero!\n"; return NULL;}
+  else /*hierExtrapFinalTrack*/tempVector = (tempVector / 2.); //account for double counting
+
+  //return hierExtrapFinalTrack.Mag();
+  return tempVector;
+}
+
+
+int computeIterExtrapFinalTracks(Vector tempDemoExtrap, Vector tempHierExtrap){
+
+if(tempDemoExtrap.Mag()==0 || tempDemoExtrap==NULL || tempHierExtrap.Mag()==0 || tempHierExtrap==NULL){ cerr<"No input demo/hier final track\n"; return -1;}
+  //Vector tempDemoExtrap = demoExtrapFinalTrack;
+  //Vector tempHierExtrap = hierExtrapFinalTrack;
+
+  int demoCount = 0;
+  int hierCount = 0;
+  int count = 0;
+  double demoAngle, hierAngle;
+
+  //Iterate demo extrap track
+  do {
+
+    if( buildEventOrthoTracks(tempDemoExtrap)<0 ){ cerr<<"Re-building ortho tracks error\n"; return -1};
+    iterDemoExtrapFinalTrack = computeDemoExtrapFinalTrack();
+    if( iterDemoExtrapFinalTrack != NULL ){
+      demoAngle = tempDemoExtrap.Angle(iterDemoExtrapFinalTrack) * TMath::RadToDeg();
+      //hierAngle = tempHierExtrap.Angle(hierExtrapFinalTrack) * TMath::RadToDeg();
+      tempDemoExtrap = iterDemoExtrapFinalTrack;
+      //tempHierExtrap = hierExtrapFinalTrack;
+    } else {
+      cerr<<"compute demo extrapolated final track error\n", return -1;
+    }
+    demoCount++;
+  } while(demoAngle < angleThreshold || demoCount < maxIteration); //Stopping condition: when the space angle converges or when done max iterations
+
+  //Iterate hier extrap track
+  do {
+
+    if( buildEventOrthoTracks(tempHierExtrap)<0 ){ cerr<<"Re-building ortho tracks error\n"; return -1};
+    iterHierExtrapFinalTrack = computeHierExtrapFinalTrack();
+    if( iterHierExtrapFinalTrack != NULL ){
+      //demoAngle = tempDemoExtrap.Angle(iterDemoExtrapFinalTrack) * TMath::RadToDeg();
+      hierAngle = tempHierExtrap.Angle(iterHierExtrapFinalTrack) * TMath::RadToDeg();
+      //tempDemoExtrap = iterDemoExtrapFinalTrack;
+      tempHierExtrap = iterHierExtrapFinalTrack;
+    } else {
+      cerr<<"compute hier extrapolated final track error\n", return -1;
+    }
+    hierCount++;
+  } while(hierAngle < angleThreshold || hierCount < maxIteration); //Stopping condition: when the space angle converges or when done max iterations
+
+  cout<<"Demo count: "<<demoCount<<" Demo angle: "<<demoAngle<<endl;
+  cout<<"Hier count: "<<hierCount<<" Hier angle: "<<hierAngle<<endl;
+
+  return 0;
+}
+
+void trackEngine::initialize(){
+
+  this->clear();
+  this->setAngleThreshold(1.);
+  this->setMaxIteration(100);
+
+}
+
+void trackEngine::setAngleThreshold(double ang_thres){
+
+    angleThreshold = ang_thres;
+
+}
+
+void trackEngine::setMaxIteration(int max_iter){
+
+  maxIteration = max_iter;
+
+}
+
+void clear(){
+
+  baselinTracks.clear();
+  baselineTrackTimes.clear();
+  eventOrthoTracks.clear();
+
+  goodTrackCount = badTrackCount = 0;
+  free(trackRank);
+  free(trackSNRArray);
+  free(cosine);
+
+  demoFinalTrack = hierFinalTrack = demoExtrapFinalTrack = hierExtrapFinalTrack
+  = iterDemoExtrapFinalTrack = iterHierExtrapFinalTrack = NULL;
+
+  angleThreshold = 0.;
+  maxIteration = 0;
+
+}
+
+int computeAllTracks(const vector< vector<double> >& antLocation, vector<TGraph *> unpaddedEvent){
+
+  initialize();
+
+  if( buildBaselineTracks(antLocation) < 0 ){ cerr<<"Build baseline tracks error\n"; return -1;}
+  if( computeFinalTracks(unpaddedEvent) < 0 ){ cerr<<"Compute final tracks error\n"; return -1;}
+  demoExtrapFinalTrack = computeDemoExtrapFinalTrack();
+  hierExtrapFinalTrack = computeHierExtrapFinalTrack();
+  if(demoExtrapFinalTrack.Mag() == 0 || demoExtrapFinalTrack==NULL){ cerr<<"No demoExtrapFinalTrack\n"; return -1;}
+  if(hierExtrapFinalTrack.Mag() == 0 || hierExtrapFinalTrack==NULL){ cerr<<"No hierExtrapFinalTrack\n"; return -1;}
+  if( computeIterExtrapFinalTracks(demoExtrapFinalTrack, hierExtrapFinalTrack) < 0){ cerr<<"Compute iter extrap final tracks error\n"; return -1;}
+
+  clear();
+
+  return 0;
+}
---
+++ trackEngine.h
@@ -0,0 +1,87 @@
+#ifndef TRACKENGINE_H
+#define TRACKENGINE_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include "TObject.h"
+#include "Vector.h"
+
+#include "RawIcrrStationEvent.h"
+#include "RawAtriStationEvent.h"
+#include "UsefulAraStationEvent.h"
+#include "UsefulIcrrStationEvent.h"
+#include "UsefulAtriStationEvent.h"
+#include "AraGeomTool.h"
+
+#include "recoTools.h"
+
+using namespace std;
+
+class trackEngine: public TObject
+{
+
+private:
+
+protected:
+
+public:
+
+   trackEngine();
+   ~trackEngine();
+
+   vector< vector<Vector> > baselinTracks;
+   vector< vector<double> > baselineTrackTimes;
+   vectot< vector<Vector> > eventOrthoTracks; //conjecture tracks, each orthogonal to its baseline track;
+   //vector<union> rank;
+/*
+   int buildBaselineTracks(const Detector *detector);
+   int buildBaselineTracks(const RawAraStationEvent *rawEvPtr);
+*/
+   int getChannelPeakTime(vector<TGraph *> unpaddedEvent, double *peakTArray);
+
+   int buildBaselineTracks(const vector< vector<double> >& antLocation);
+   double checkBaselineTracks();
+   int buildEventOrthoTracks(Vector finalTrack);
+
+   int goodTrackCount, badTrackCount;
+   int *trackRank;
+   float *trackSNRArray;
+   double *cosine;
+
+   Vector demoFinalTrack;
+   Vector hierFinalTrack;
+   Vector demoExtrapFinalTrack;
+   Vector hierExtrapFinalTrack;
+   Vector iterDemoExtrapFinalTrack;
+   Vector iterHierExtrapFinalTrack;
+
+   int computeFinalTracks(vector<TGraph *> unpaddedEvent);
+   //int computeExtrapFinalTracksWithDemo();
+   //int computeExtrapFinalTracksWithHier();
+   //int computeExtrapFinalTracks(Vector finalTrack);
+   Vector computeDemoExtrapFinalTrack();
+   Vector computeHierExtrapFinalTrack();
+
+
+   double angleThreshold;
+   int maxIteration;
+   //int computeIterExtrapFinalTracksWithDemo();
+   //int computeIterExtrapFinalTracksWithHier();
+   int computeIterExtrapFinalTracks(Vector tempDemoExtrap, Vector tempHierExtrap);
+   void setAngleThreshold(double ang_thres);
+   void setMaxIteration(int max_iter);
+
+   void initialize();
+   void clear();
+
+   int computeAllTracks(const vector< vector<double> >& antLocation, vector<TGraph *> unpaddedEvent);
+
+   ClassDef(trackEngine, 1);
+
+}
+
+#endif
