e9a16c7ead703961cebde2176d7cee5c6fad2884
Ming-Yuan Lu <ming-yuanlu@MBP-FVFVP117HV2D.local>
2017-12-24 23:59:25 -0600

    Add linkdef_trackEngine.h

---
+++ linkdef_trackEngine.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//#pragma link C++ class recoSettings+;
+//#pragma link C++ class Healpix_Onion+;
+#pragma link C++ class trackEngine+;
+
+
+#endif // __CINT__
