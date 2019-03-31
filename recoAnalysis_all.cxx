2625a5919829265a45107cdcaee6d9219d13de0e
Ming-Yuan Lu <ming-yuanlu@MBP-FVFVP117HV2D.local>
2019-03-31 11:06:44 -0500

    Debug

--- recoAnalysis_all.cxx
+++ recoAnalysis_all.cxx
@@ -283,7 +283,7 @@ if (list.is_open() ){
       event = stoi(line);
       //getline(list, line, '\n');
 
-      if(event >= 14){
+      if(event >= 2){
          //cout<<"run: "<<run/*<<" event: "<<event*/<<endl;
          cout<<"run: "<<run<<" event: "<<event<<endl;
          listOfRuns.push_back(run);
