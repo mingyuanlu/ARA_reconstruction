feefcb63039921ff2452a05f5390037cba1e153e
Ming-Yuan Lu <ming-yuanlu@MBP-FVFVP117HV2D.local>
2019-03-31 10:28:22 -0500

    Debug

--- recoAnalysis_all.cxx
+++ recoAnalysis_all.cxx
@@ -283,7 +283,7 @@ if (list.is_open() ){
       event = stoi(line);
       //getline(list, line, '\n');
 
-      if(event >= 12){
+      if(event >= 13){
          //cout<<"run: "<<run/*<<" event: "<<event*/<<endl;
          cout<<"run: "<<run<<" event: "<<event<<endl;
          listOfRuns.push_back(run);
