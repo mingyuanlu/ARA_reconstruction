b61b719b2c579c08aace39e3b1b36a9966f5d1ad
Ming-Yuan Lu <ming-yuanlu@MBP-FVFVP117HV2D.local>
2019-03-31 10:30:38 -0500

    Debug

--- recoAnalysis_all.cxx
+++ recoAnalysis_all.cxx
@@ -283,7 +283,7 @@ if (list.is_open() ){
       event = stoi(line);
       //getline(list, line, '\n');
 
-      if(event >= 13){
+      if(event >= 14){
          //cout<<"run: "<<run/*<<" event: "<<event*/<<endl;
          cout<<"run: "<<run<<" event: "<<event<<endl;
          listOfRuns.push_back(run);
