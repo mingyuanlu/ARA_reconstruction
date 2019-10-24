e0bf434ef685c23c511458bba27b75ec83782530
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-10-24 17:47:52 -0500

    Implement calpulser filter

--- recoTools.h
+++ recoTools.h
@@ -344,6 +344,8 @@ int reconstruct3DXCorrEnvelopeGetMaxPixAndMapData_2ndRayReco(recoSettings *setti
                     recoData *summary, char *filename, float *mapData);
 int reconstruct3DXCorrEnvelopeGetMaxPixAndMapData_constantNFilter(recoSettings *settings, vector<TGraph *>& cleanEvent,
                                                                   recoEnvData *clEnv, float *recoDelays, float *recoDelays_V, float *recoDelays_H, const int *chanMask, recoData *summary, char *filename/*, float *mapData*/);
+int reconstruct3DXCorrEnvelopeGetMaxPixAndMapData_calpulerFilter(recoSettings *settings, vector<TGraph *>& cleanEvent,
+                                                                  recoEnvData *clEnv, float *recoDelays, float *recoDelays_V, float *recoDelays_H, const int *chanMask, recoData *summary/*, char *filename*//*, float *mapData*/);
 int reconstruct3DXCorrEnvelopeGetMaxPix_ZoomMode(recoSettings *settings, vector<TGraph *>& cleanEvent, recoEnvData *clEnv,
                     const float stationCenterDepth, const vector<vector<double> >& antLocation,
                     float *recoDelays, float *recoDelays_V, float *recoDelays_H, const int *chanMask,
