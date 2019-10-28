976686aebd718a9d35090c1453a0c86e82cb50e6
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-10-27 20:09:27 -0500

    Debug

--- analysis.cxx
+++ analysis.cxx
@@ -1270,7 +1270,7 @@ for (Long64_t ev=0; ev<runEventCount; ev++){
    }
 
    /* A3 spikey D1 filter */
-   if (isSpikeyStringEvent(statiodId, dropARA03D4, snrArray, grFFT, spikeyRatio)){
+   if (isSpikeyStringEvent(stationdId, dropARA03D4, snrArray, grFFT, spikeyRatio)){
 
       corruptD1EventCount+=1;
       unpaddedEvent.clear();
